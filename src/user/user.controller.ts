f57d3eed5b6e50341de621c6fe8430f06999c054
Hill <hillonyechekwa@gmail.com>
2024-10-09 23:37:04 +0100

    added routes and services methods for the user module made updates to auth controller and auth service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,41 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/createuser.dto';
 import { UpdateUserDto } from './dto/updateuser.dto';
 
 
-import { ApiTags, ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { ApiTags, ApiOkResponse } from '@nestjs/swagger';
+import { UserEntity } from './user.entity';
 @Controller('user')
 @ApiTags('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return
-    // return this.userService.create(createUserDto);
-  }
 
   @Get()
+  @ApiOkResponse({type: UserEntity, isArray: true})
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return
+  @ApiOkResponse({type: UserEntity})
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id)
     // return this.userService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return
+  @ApiOkResponse({type: UserEntity})
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateProfile(id, updateUserDto)
     // return this.userService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return
+  @ApiOkResponse({type: UserEntity})
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.deleteUser(id)
     // return this.userService.remove(+id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,7 +5,8 @@ import * as bcrypt from 'bcrypt'
 // import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/createuser.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Prisma, User} from '@prisma/client';
+import { User} from '@prisma/client';
+import { UpdateUserDto } from './dto/updateuser.dto';
 
 
 @Injectable()
@@ -17,13 +18,27 @@ export class UserService {
       private prisma: PrismaService
     ){}
 
-    async findOne(data: Partial<User>): Promise<User>{
+    async findOneAuth(data: Partial<User>): Promise<User>{
         const user = await this.prisma.user.findUnique({
             where: {
                 email: data.email
             }
         })
         if (!user) {
+            throw new UnauthorizedException("user couldn't be found!!")
+        }
+
+        return user
+    }
+
+    async findOne(userId: number): Promise<User> {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+
+        if (!user) { //* just to note. this might be tots unnecessary
             throw new UnauthorizedException("user couldn't be found!!")
         }
 
@@ -44,10 +59,29 @@ export class UserService {
     }
 
 
-    findAll() {
-        return "found all users"
+    findAll():Promise<User[]> {
+        return this.prisma.user.findMany({})
     }
 
 
-    async updateProfile () {}
+    async updateProfile(userId: number, updateUserDto: UpdateUserDto): Promise<User> {
+        //*TODO FIX THIS  UPDATE MODEL NOT THE USER MODEL DIRECTLY
+        const updateUser = await this.prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: updateUserDto
+        })
+
+        return updateUser
+    }
+
+
+    async deleteUser(userId: number) {
+        return await this.prisma.user.delete({
+            where: {
+                id: userId
+            }
+        })
+    }
 }
