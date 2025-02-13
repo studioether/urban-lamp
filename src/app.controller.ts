b35d7744f0a6faa519664747c7cf4f29983a6afc
Hill <hillonyechekwa@gmail.com>
2025-02-13 17:57:56 +0100

    updated refresh-auth-guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, BadRequestException, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, BadRequestException } from '@nestjs/common';
 import { AppService } from './app.service';
 // import { RefreshJwtAuthGuard } from './guards/refresh-auth.guard';
 // import { JwtAuthGuard } from './guards/jwt.guard';
